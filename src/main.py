748cf73885983de33df91446879a4bdeced65580
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-07-11 16:07:05 -0700

    Add compatible snake_casing option in python

--- src/main.py
+++ src/main.py
@@ -1,8 +1,17 @@
+#!/usr/bin/env python3
 import fileinput
+import re
+
+WORD_BOUNDARY_PATTERN = r"\b|(?<=[a-z])(?=[A-Z])|(?<=[A-Z0-9])(?=[A-Z][a-z])"
 
 
 def snake_case(line):
-    return line
+    # Split a line based on the defined word boundaries. This will contain empty
+    # spaces before and after the line. Underscores are considered as part of
+    # word boundary in the predefined `\b`.
+    words = re.split(WORD_BOUNDARY_PATTERN, line)
+    cased = [w.strip("_").lower() for w in words if w]
+    return "_".join(cased)
 
 
 def main():
