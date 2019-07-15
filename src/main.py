ec735a761475fc4de618b4d3298a86460c26aeae
Anthony Miyaguchi <amiyaguchi@mozilla.com>
2019-07-15 11:54:55 -0700

    Add reference regex implementation

--- src/main.py
+++ src/main.py
@@ -1,24 +1,30 @@
 #!/usr/bin/env python3
 import fileinput
 import re
-import string
 
-WORD_BOUND_PAT = re.compile(
+# Search for all camelCase situations in reverse with arbitrary lookaheads.
+REV_WORD_BOUND_PAT = re.compile(
     r"""
     \b                                  # standard word boundary
-    |(?<=[a-z])(?=[A-Z])                # aA -> a|A boundary
-    |(?<=[A-Z])(?=[A-Z][a-z])        # AAAa -> AA|Aa boundary
+    |(?<=[A-Z])(?=[a-z])                # aA -> a|A boundary
+    |(?<=[a-z][A-Z])(?=[A-Z])           # AAa -> A|Aa boundary
+    |(?<=[a-z][A-Z])(?=\d*[A-Z])        # A7Aa -> A7|Aa boundary
+    |(?<=[a-z][A-Z])(?=\d*[a-z])        # a7Aa -> a7|Aa boundary
+    |(?<=[A-Z])(?=\d*[a-z])             # a7A -> a7|A boundary
     """,
     re.VERBOSE,
 )
 
 
 def snake_case(line: str) -> str:
-    # replace non-alphanumeric characters with spaces
-    subbed = re.sub(r"[^\w]|_", " ", line)
-    words = WORD_BOUND_PAT.split(subbed)
-    # filter space between words and snake_case
-    return "_".join([w.lower() for w in words if w.strip()])
+    # replace non-alphanumeric characters with spaces in the reversed line
+    subbed = re.sub(r"[^\w]|_", " ", line[::-1])
+
+    # apply the regex on the reversed string
+    words = REV_WORD_BOUND_PAT.split(subbed)
+
+    # filter spaces between words and snake_case and reverse again
+    return "_".join([w.lower() for w in words if w.strip()])[::-1]
 
 
 def main():
